73c697d80c3e8dd5b63eea979c92f4c1a586b19e
schneider-chris <christine.schneider@escola.pr.gov.br>
2025-04-08 21:35:21 -0300

    Add files via upload

--- ex30.py
+++ ex30.py
@@ -28,3 +28,31 @@ def cadastrar_pessoa(cadastros):
     cadastros.append({"Nome": nome, "Idade": idade, "Turma": turma, "Curso": curso})
     salvar_cadastros(cadastros)
     print("Cadastro finalizado com sucesso")
+
+    def ver_cadastros(cadastros):
+        if not cadastros:
+            print("\n Nenhum cadastro no sistema")
+        else:
+            print("\n-------Lista de Cadastros------")
+            for i, pessoa in enumerate(cadastros, 1):
+                print(f"{i} . Nome: {pessoa['Nome']}, Idade: 
+{pessoa['Idade']}, Turma: {pessoa['Turma']}, Curso: {pessoa['Curso']}")
+    input("\n Pressione Enter para voltar ao menu...")
+
+def main():
+    cadastros = carregar_cadastros()
+    while True:
+        exibir_menu()
+        opcao = input("Escolha uma opção:")
+        if opcao == "1":
+            cadastrar_pessoa(cadastros)
+        elif opcao == "2":
+            ver_cadastros(cadastros)
+        elif opcao == "3":
+            print("Obrigado por usar o sistema de cadastro!")
+            break
+        else:
+            print("Opção inválida! Tente Novamente.")
+
+if __name__=="__main__":
+    main()
